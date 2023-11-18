0c4524e20c63dd959001d4e2a76aea9b7fe37b2f
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-18 12:36:56 +0000

    3. 시계 만들기

---
+++ js/clock.js
@@ -0,0 +1,22 @@
+
+/**
+ * document에 있는 id가 clock인 요소
+ */
+const clock = document.querySelector("h2#clock");
+
+/**
+ * 시간을 구하는 함수
+ */
+function getClock(){
+    
+    const date = new Date()
+    const hour = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    
+    clock.innerText = `${hour}:${minutes}:${seconds}`;
+}
+
+//setInterval() 이 1초 후 시작하므로 처음에 호출을 해 준다.
+getClock();
+setInterval(getClock, 1000);
