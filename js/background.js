2f237866a059286cf5a7b2c1f63b943e3571828d
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-18 13:18:27 +0000

    4. 명언 및 배경 출력하기

---
+++ js/background.js
@@ -0,0 +1,18 @@
+/**
+ * 화면에 배경 랜덤으로 출력하기
+ */
+
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg", "11.jpg"];
+//위의 코드에서 파일명은 자신이 갖고 있는 파일의 이름을 적는다.
+
+//화면에 출력할 이미지를 랜덤으로 선택한다.
+const selectedImage = images[Math.floor(Math.random() * images.length)];
+
+//html문서에 img요소를 만든다.
+const bgImage = document.createElement("img");
+
+//만든 img요소의 프로퍼티를 지정한다.
+bgImage.src = `img/${selectedImage}`;
+
+//만든 요소를 문서에 붙인다.
+document.body.appendChild(bgImage);
